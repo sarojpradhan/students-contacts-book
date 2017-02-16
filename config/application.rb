dcd075a4414171ddbc7daf55b9bb032f6fc4a26e
Saroj Pradhan <20saroj@gmail.com>
2017-02-16 17:38:27 +0200

    mailer setup

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,15 @@ module StudentContactBook
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    ActionMailer::Base.smtp_settings = {
+      address:        'smtp.gmail.com',
+      domain:         'mail.google.com',
+      port:           587,
+      user_name:      'sarojmailer@gmail.com',
+      password:       'mypassg9',
+      authentication:  'login',
+      enable_starttls_auto: true
+    }
   end
 end
